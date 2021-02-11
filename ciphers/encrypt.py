147396428df97139ae8017274a207054fa5ad03d
kayn0111034 <0111034@student.isf.edu.hk>
2021-02-12 01:38:56 +0800

    moved the program part of encrypt to cipher_test

--- ciphers/encrypt.py
+++ ciphers/encrypt.py
@@ -27,23 +27,6 @@ def caesar_cipher(text,rotate_num):
     return text.translate(str.maketrans(string.ascii_uppercase,upper)).translate(str.maketrans(string.ascii_lowercase,lower)).translate(str.maketrans(string.digits,digit))
 
 
-print("(0) Caesar Cipher")
-choose=int(input("Which cipher do you want to use?"))
-
-if choose==0:
-    text=input("Please enter text for the encryption: ")
-    rotate_num=int(input("How many times would you like to rotate your text?"))
-    start = time.time()
-    print(caesar_cipher(text,rotate_num))
-    end = time.time()
-    print(f"It took {end-start} seconds to encrypt your text")
-else:
-    text=input("Please enter text for the encryption: ")
-    rotate_num=int(input("How many times would you like to rotate your text?"))
-    start = time.time()
-    print(caesar_cipher(text,rotate_num))
-    end = time.time()
-    print(f"It took {end-start} seconds to encrypt your text")
 
 
 
