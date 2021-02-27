d9d27b61bba856e8d292b820035215a9791d9018
kayn0111034 <0111034@student.isf.edu.hk>
2021-02-28 00:25:09 +0800

    Rever previous commit, it was pushed to wrong directory
    This reverts commit 46b943d129c40c518d48348c9c681381aedfca9a.

--- password-generator/password_generator.py
+++
@@ -1,7 +0,0 @@
-from password import password_gen
-
-
-print("Create a randomly generated password!")
-letter_amount=int(input("Input how long you would like your password to be:"))
-
-password_gen(letter_amount)
